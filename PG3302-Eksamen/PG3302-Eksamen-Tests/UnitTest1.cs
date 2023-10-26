a2a9b1d10872985409b42aa1859d000286542db6
Mathias <mato058@student.kristiania.no>
2023-10-26 11:45:32 +0200

    Linked test-project

---
+++ PG3302-Eksamen/PG3302-Eksamen-Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework.Internal;
+using PG3302_Eksamen;
+
+namespace PG3302_Eksamen_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+          
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
