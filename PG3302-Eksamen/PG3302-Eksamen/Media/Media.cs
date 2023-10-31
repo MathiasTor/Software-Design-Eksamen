ebcd429d3c786ea501f99ec8eba2ac5158cfe3f1
Mathias <mato058@student.kristiania.no>
2023-10-31 11:07:36 +0100

    Update Media.cs

--- PG3302-Eksamen/PG3302-Eksamen/Media/Media.cs
+++ PG3302-Eksamen/PG3302-Eksamen/Media/Media.cs
@@ -10,7 +10,7 @@ namespace PG3302_Eksamen.Media
     {
         //Properties
         public string Title { get; set; }
-        public string Author { get; set; }
+        public string Creator { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
 
