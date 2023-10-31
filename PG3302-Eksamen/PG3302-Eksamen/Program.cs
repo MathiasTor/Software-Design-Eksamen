7aa5631d15c48530226d965667a677ea39192c89
Mathias <mato058@student.kristiania.no>
2023-10-31 13:29:54 +0100

    SampleData - books in main

--- PG3302-Eksamen/PG3302-Eksamen/Program.cs
+++ PG3302-Eksamen/PG3302-Eksamen/Program.cs
@@ -1,4 +1,5 @@
-﻿using PG3302_Eksamen.UI;
+﻿using PG3302_Eksamen.Logic;
+using PG3302_Eksamen.UI;
 
 namespace PG3302_Eksamen
 {
@@ -6,6 +7,12 @@ namespace PG3302_Eksamen
     {
         static void Main(string[] args)
         {
+            //Sample-data - for testing purposes
+            BookLogic bookLogic = new();
+            bookLogic.AddBook("Harry Potter", "J.K. Rowling", 1997, "Fantasy", 300);
+            bookLogic.AddBook("The Lord of the Rings", "J.R.R. Tolkien", 1954, "Fantasy", 500);
+            bookLogic.AddBook("The Hobbit", "J.R.R. Tolkien", 1937, "Fantasy", 300);
+
 
             ConsoleApp mainMenu = new();
             mainMenu.RunProgram();             
