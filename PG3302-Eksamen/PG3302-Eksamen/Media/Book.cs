1ed715f8a5fa5d86bb4f3e0d048ba722b972980d
Mathias <mato058@student.kristiania.no>
2023-10-31 11:00:28 +0100

    Book class created

---
+++ PG3302-Eksamen/PG3302-Eksamen/Media/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG3302_Eksamen.Media
+{
+    public class Book : Media
+    {
+        //Properties
+        public int Pages { get; set; }
+
+        //Constructor
+        public Book(string title, string author, int releaseYear, string genre, int pages)
+        {
+            Title = title;
+            Author = author;
+            ReleaseYear = releaseYear;
+            Genre = genre;
+            Pages = pages;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}, Release Year: {ReleaseYear}, Genre: {Genre}, Pages: {Pages}";
+        }
+
+    }
+}
