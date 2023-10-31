140396ed95174a58ed96880913055b5fc225323e
Mathias <mato058@student.kristiania.no>
2023-10-31 12:33:24 +0100

    Update BookLogic.cs

--- PG3302-Eksamen/PG3302-Eksamen/Logic/BookLogic.cs
+++ PG3302-Eksamen/PG3302-Eksamen/Logic/BookLogic.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,16 @@ namespace PG3302_Eksamen.Logic
     public class BookLogic
     {
         public List<Book> Books { get; set; }
+        public BookLogic()
+        {
+            Books = new List<Book>();
+        }
+
         public void AddBook(string title, string creator, int releaseYear, string genre, int pages)
         {
-            Book bookToAdd = new Book(title, creator, releaseYear, genre, pages);
+            //Book bookToAdd = new Book(title, creator, releaseYear, genre, pages);
 
-            Books.Add(bookToAdd);
+            Books.Add(new Book(title, creator, releaseYear, genre, pages));
         }
 
         public void RemoveBook(String title)
