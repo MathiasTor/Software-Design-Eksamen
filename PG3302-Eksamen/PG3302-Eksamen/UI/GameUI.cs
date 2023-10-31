75b64260dadbdac1b9ffd320f47a62d12ee0d368
maiwand0314 <143669044+maiwand0314@users.noreply.github.com>
2023-10-31 14:32:43 +0100

    "fikset på GameUI, og addet GameLogic filen med Logic"

--- PG3302-Eksamen/PG3302-Eksamen/UI/GameUI.cs
+++ PG3302-Eksamen/PG3302-Eksamen/UI/GameUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PG3302_Eksamen.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,22 @@ using System.Threading.Tasks;
 
 namespace PG3302_Eksamen.UI
 {
-    internal class GameUI
+    public class GameUI
     {
+        
+        public GameLogic gameLogic = new();
         public void GameMenu()
         {
 
             Console.WriteLine("\n\nWelcome to the game registry\n" +
-                "Please Choose what you would like to do.\n" +
-                "1. Display games.\n" +
-                "2. Add a new game.\n" +
-                "3. Edit existing game.\n" +
-                "4. Delete a game.\n\n" +
-                "--------------------------\n\n" +
-                "9. Back to Main Menu\n" +
-                "0. To exit");
+                              "Please Choose what you would like to do.\n" +
+                              "1. Display games.\n" +
+                              "2. Add a new game.\n" +
+                              "3. Edit existing game.\n" +
+                              "4. Delete a game.\n\n" +
+                              "--------------------------\n\n" +
+                              "9. Back to Main Menu\n" +
+                              "0. To exit");
 
             Console.Write("\nPlease enter your input here: ");
 
@@ -32,19 +35,77 @@ namespace PG3302_Eksamen.UI
                     DisplayGames();
                     break;
 
+                case 2:
+                    AddGame();
+                    break;
+
+                case 3:
+                    EditGame();
+                    break;
+
+                case 4:
+                    DeleteGame();
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input! (1,2,3,4,0)");
+                    GameMenu();
+                    break;
             }
-
-
         }
 
         public void DisplayGames()
         {
+            gameLogic.DisplayGames();
 
-            Console.WriteLine("test");
+            GameMenu();
 
         }
 
+        private void AddGame()
+        {
 
+            Console.Write("Game title: ");
+            string title = Console.ReadLine();
 
+            Console.Write("Game Creator: ");
+            string creator = Console.ReadLine();
+
+            Console.Write("Release Year: ");
+            int releaseYear = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Game Genre: ");
+            string genre = Console.ReadLine();
+            
+            Console.WriteLine("Game Platform: ");
+            string platform = Console.ReadLine();
+
+            gameLogic.AddGame(title, creator, releaseYear, genre, platform);
+
+            gameLogic.DisplayGames();
+
+            GameMenu();
+        }
+
+        private void EditGame()
+        {
+            Console.Write("Enter the title of the game you wish to edit: ");
+            string? oldTitle = Console.ReadLine();
+            Console.Write("Enter the new title. ");
+            string? newTitle = Console.ReadLine();
+            gameLogic.EditGameTitle(oldTitle, newTitle);
+            GameMenu();
+        }
+
+        private void DeleteGame()
+        {
+            Console.Write("Enter the Title of the Game you wish to delete: ");
+            string userInput = Console.ReadLine();
+            gameLogic.RemoveGame(userInput);
+            GameMenu();
+        }
     }
 }
