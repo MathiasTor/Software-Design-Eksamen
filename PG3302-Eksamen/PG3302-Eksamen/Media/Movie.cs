a25b9f055170813b63d2172c2c6e76ef264a1eed
Mathias <mato058@student.kristiania.no>
2023-10-31 11:07:43 +0100

    Create Movie.cs

---
+++ PG3302-Eksamen/PG3302-Eksamen/Media/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG3302_Eksamen.Media
+{
+    public class Movie : Media
+    {
+        //Properties
+        private int LengthInMinutes { get; set; }
+        
+        //Constructor
+        public Movie(string title, string creator, int releaseYear, string genre, int lengthInMinutes)
+        {
+            Title = title;
+            Creator = creator;
+            ReleaseYear = releaseYear;
+            Genre = genre;
+            LengthInMinutes = lengthInMinutes;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return $"Title: {Title}, Director: {Creator}, Release Year: {ReleaseYear}, Genre: {Genre}, Length in minutes: {LengthInMinutes}";
+        }
+    }
+}
