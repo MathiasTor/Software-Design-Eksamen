032394b3a877b71824385d3a382d54c1c423c8db
Mathias <mato058@student.kristiania.no>
2023-10-31 11:17:40 +0100

    Game & media

--- PG3302-Eksamen/PG3302-Eksamen/Media/Media.cs
+++ PG3302-Eksamen/PG3302-Eksamen/Media/Media.cs
@@ -9,10 +9,10 @@ namespace PG3302_Eksamen.Media
     public abstract class Media
     {
         //Properties
-        public string Title { get; set; }
-        public string Creator { get; set; }
+        public string? Title { get; set; }
+        public string? Creator { get; set; }
         public int ReleaseYear { get; set; }
-        public string Genre { get; set; }
+        public string? Genre { get; set; }
 
     }
 }
