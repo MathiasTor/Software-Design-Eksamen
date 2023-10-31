fbf0e858ae78906e58c119e92355d5c1b2004c04
BorseHaraldsen <Augustborse@gmail.com>
2023-10-31 10:53:49 +0100

    Made class for menu / console. Added start draft of some console.

---
+++ PG3302-Eksamen/PG3302-Eksamen/UI/ConsoleApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG3302_Eksamen.UI
+{
+    public class ConsoleApp
+    {
+
+        public void runProgram() {
+
+            Console.WriteLine("Welcome the the Library of Media!\n" +
+                "Please choose what type of media you would like to access: ");
+
+
+        }
+
+
+
+    }
+}
