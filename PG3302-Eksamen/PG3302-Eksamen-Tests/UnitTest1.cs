574846ff70a468ff2a20e16eec69a8b4ebe717dd
BorseHaraldsen <Augustborse@gmail.com>
2023-10-31 13:59:12 +0100

    Completely lost no idea tests LOL

--- PG3302-Eksamen/PG3302-Eksamen-Tests/UnitTest1.cs
+++ PG3302-Eksamen/PG3302-Eksamen-Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework.Internal;
 using PG3302_Eksamen;
-
+using PG3302_Eksamen.Logic;
+using BookLogic;
 namespace PG3302_Eksamen_Tests
 {
     public class Tests
@@ -14,6 +15,10 @@ namespace PG3302_Eksamen_Tests
         [Test]
         public void Test1()
         {
+
+            var BookLogic = new BookLogic();
+
+
         }
     }
 }
