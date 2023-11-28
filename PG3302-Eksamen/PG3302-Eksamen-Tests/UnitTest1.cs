fe6cd844e013f1aecaa5274043f8068ba75a8044
BorseHaraldsen <Augustborse@gmail.com>
2023-11-28 13:17:54 +0100

    Unittest

--- PG3302-Eksamen/PG3302-Eksamen-Tests/UnitTest1.cs
+++ PG3302-Eksamen/PG3302-Eksamen-Tests/UnitTest1.cs
@@ -1,22 +1,91 @@
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.VisualBasic;
 using NUnit.Framework.Internal;
 using PG3302_Eksamen.Logic;
+using PG3302_Eksamen.Media;
+using System.Collections;
+using static System.Formats.Asn1.AsnWriter;
+using System.Transactions;
+using PG3302_Eksamen.Database;
+
 namespace PG3302_Eksamen_Tests
 {
     public class Tests
     {
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
-          
+            var options = MediaDbContextFactory.Options();
+
+            using (var db = new MediaDbContext(options))
+            {
+                foreach (Book book in db.Books)
+                {
+                    db.Remove(book);
+                    db.SaveChanges();
+                }
+            }
+
+
         }
 
+        [TearDown] public void TearDown() {
+           
+        }
+
+
+        
         [Test]
-        public void Test1()
+        public void AddBook()
         {
+            using (TransactionScope scope = new TransactionScope()) ;
 
-            var BookLogic = new BookLogic();
+                // Arrange
+            string Title = "testingname";
+            string Creator = "tolkien";
+            int ReleaseYear = 1943;
+            string Genre = "fantasy";
+            int Pages = 800;
+            BookLogic bookLogic = new BookLogic();
+
+            // Act
+            bookLogic.AddBook(Title, Creator, ReleaseYear, Genre, Pages);
+
+
+            // Asssert
+            Assert.That(bookLogic.CheckIfBookExists("testingname"));
+
+
 
 
         }
+    
+        [Test]
+        public void GetAllBooks() {
+
+            string Title = "testingname";
+            string Creator = "tolkien";
+            int ReleaseYear = 1943;
+            string Genre = "fantasy";
+            int Pages = 800;
+
+
+            Book bookTest = new Book(Title, Creator, ReleaseYear, Genre, Pages);
+            DbLogic database = new();
+
+
+            ArrayList bookList = database.GetAllBooks();
+            foreach (var book in bookList)
+            {
+                Console.WriteLine(book);
+            }
+
+
+            Assert.That(bookList.Contains(bookTest), Is.True);
+        }
+     
     }
+   
+
 }
