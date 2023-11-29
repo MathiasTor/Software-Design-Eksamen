fe344af49418e6ab491b4fb770655f9c4002f65c
Mathias <mato058@student.kristiania.no>
2023-11-29 19:09:44 +0100

    Removed ? from User

--- PG3302-Eksamen/PG3302-Eksamen/User/SystemUser.cs
+++ PG3302-Eksamen/PG3302-Eksamen/User/SystemUser.cs
@@ -7,9 +7,9 @@ namespace PG3302_Eksamen.User
     {
         //Properties
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
         public bool IsAdmin { get; set; }
 
         //Constructors
